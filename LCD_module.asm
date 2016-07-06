2a16afa6afa6f908537583405cad23c8852d0f82
garyyap123 <mmugary123@gmail.com>
2016-07-07 03:34:38 +0800

    Added LCD interface code.

---
+++ LCD_module.asm
@@ -0,0 +1,109 @@
+/*
+	Author : Gary Yap Hon Shin
+	Created: 7/7/2016 (d-m-y)
+	
+	The following is LCD module code.
+	This code uses the LCD 4bit mode feature,
+	as this will dramatically reduce number of 
+	physcial wire required. PORT 2 (7-4)is the data bus,
+	where port 1.0 = EN and port 1.1 = RS
+*/
+
+
+	 
+	  RS equ P1.1; //RS = 1 DATA , RS = 0 COMMAND
+	  EN equ P1.0;
+	  Upper_nibble equ 35H;
+	  Lower_nibble equ 36H;
+
+	  ORG 0000H
+
+	  MOV A,#20H;
+	  CLR RS;
+	  SETB EN;
+	  ACALL LCD_DELAY;
+	  CLR EN;
+	  
+	  MOV A, #28H
+	  ACALL WRITE_COMMAND;
+	  MOV A, #0CH
+	  ACALL WRITE_COMMAND;
+	  MOV A, #06H
+	  ACALL WRITE_COMMAND;
+	  MOV A, #01H
+	  ACALL WRITE_COMMAND;
+
+	  MOV 37H,#30H;
+	  ACALL WRITE_DATA;
+	
+MAIN: 
+
+	  MOV A,37H;
+	  ADD A,#01H;
+	  MOV 37H,A;
+	  ACALL WRITE_DATA;
+	  ACALL Heart_beat;
+	  ACALL CLEAR_LCD;
+	  SJMP MAIN;
+	  
+	
+Heart_beat:	 SETB P0.3;
+			 ACALL DELAY;
+			 clr  P0.3;
+			 ACALL DELAY;
+			 RET;
+			 
+CLEAR_LCD:  MOV A,#01H;
+			ACALL WRITE_COMMAND;
+			RET;
+			 
+WRITE_COMMAND: CLR RS;
+			   ACALL BIT4_FORMAT;
+			   MOV P2, Upper_nibble;
+			   SETB EN;
+			   ACALL LCD_DELAY;
+			   CLR EN;
+			   MOV P2, Lower_nibble;
+			   SETB EN;
+			   ACALL LCD_DELAY;
+			   CLR EN;
+			   RET;
+			   
+WRITE_DATA:    SETB RS;
+			   ACALL BIT4_FORMAT;
+			   MOV P2, Upper_nibble;
+			   SETB EN;
+			   ACALL LCD_DELAY;
+			   CLR EN;
+			   MOV P2, Lower_nibble;
+			   SETB EN;
+			   ACALL LCD_DELAY;
+			   CLR EN;
+			   RET;
+			  
+			  
+//Use to separate 8bit data into 4bit chuncks.
+BIT4_FORMAT: MOV Upper_nibble,A;
+			 ANL Upper_nibble,#0F0H;
+			 SWAP A;
+			 ANL A,#0F0H;
+			 MOV Lower_nibble,A;
+			 RET;
+
+//1s delay	
+DELAY:	MOV R2,#10; //1s
+DLY1 :	MOV R1,#200;//0.1s
+DLY0 :	MOV R0,#250;
+		DJNZ R0,$;//500uS
+		DJNZ R1,DLY0; 
+		DJNZ R2,DLY1;
+		RET;
+	
+//12.75mS delay
+LCD_DELAY: MOV  20H,#50;          
+DELAY2   : MOV  21H,#255;          
+DELAY3   : DJNZ 21H,DELAY3;
+           DJNZ 20H,DELAY2;
+           RET;
+		
+		   END
--- my_code.asm
+++ my_code.asm
@@ -1,24 +1,33 @@
-		LED8 DATA P0;
-		
-		ORG 0000H;
-		
-		
+/*
+  Author: Gary Yap Hon Shin 
+  Created: 6/7/2016 (d-m-y)
+  
+  This is a "Hello World" program to test
+  whether the basic requirement of the 
+  MCU is set up properly.If everything is
+  working properly, two LED at PORT0,pin2
+  and pin3 will be toggle alternatively 
+  at the rate of 0.5 second for each interval.
+*/
+	
+	LED8 DATA P0;
+	
+	ORG 0000H;
+	
 MAIN:	MOV A,#0FBH;
-		MOV LED8,A;
-		CALL DELAY;
-		RL	A;
-		MOV LED8,A;
-		CALL DELAY;
-		SJMP MAIN;
+	MOV LED8,A;
+	CALL DELAY;
+	RL	A;
+	MOV LED8,A;
+	CALL DELAY;
+	SJMP MAIN;
 		
 DELAY:	MOV R2,#5;
 DLY1:	MOV R1,#200
 DLY0:	MOV R0,#250
-		DJNZ R0,$
-		DJNZ R1,DLY0
-		DJNZ R2,DLY1
-		RET
-		END	
+	DJNZ R0,$
+	DJNZ R1,DLY0
+	DJNZ R2,DLY1
+	RET
+	END	
 		
-		
-	
